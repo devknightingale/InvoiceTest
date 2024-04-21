22eea347e36471ef7e17416b2066f439a0bb3fe6
devknightingale <107380630+devknightingale@users.noreply.github.com>
2024-04-21 15:41:28 -0400

    Add files via upload
    Assignment details: 
    (Invoice Class) Create a class called Invoice that a hardware store might use to represent an invoice for an item sold at the store. An Invoice should include four pieces of information as either instance variables or auto-implemented properties—a part number (type string), a part description (type string), a quantity of the item being purchased (type int) and a price per item (decimal). Your class should have a constructor that initializes the four values. Provide a property with a get and set accessor for any instance variables. For the Quantity and PricePerItem properties, if the value passed to the set accessor is negative, the value of the instance variable should be left unchanged. Also, provide a method named GetInvoiceAmount that calculates the invoice amount (i.e., multiplies the quantity by the price per item), then returns the amount as a decimal value. Write a test app named InvoiceTest that demonstrates class Invoice’s capabilities.

---
+++ Invoice.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.AccessControl;
+
+class Invoice 
+{
+    public string Number { get; set; } 
+    public string Description { get; set; }
+
+    private int quantity;
+    private decimal price;
+    private decimal invoiceAmount; 
+    
+    //need a constructor now? 
+    public Invoice(string partNumber, string partDescription, int partQuantity, decimal pricePerItem)
+    {
+        Number = partNumber;
+        Description = partDescription;
+        Quantity = partQuantity;
+        Price = pricePerItem;
+    }
+
+    //for quantity and price, need get and set variables 
+    
+    public int Quantity
+    {
+        get
+        {
+            return quantity;
+        }
+        private set
+        {
+            if (value > 0)
+            {
+                quantity = value; 
+            }
+        }
+    }
+    public decimal Price
+    {
+        get
+        {
+            return price;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                price = value;
+            }
+        }
+    }
+
+    // method to get invoice amount 
+    public decimal GetInvoiceAmount() 
+    { 
+        return price * quantity; 
+    }
+
+}
---
+++ InvoiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System; 
+
+class InvoiceTest
+{
+    static void Main()
+    {
+         
+        Invoice myInvoice2 = new Invoice("23", "Test description", 34, 5.44m);
+
+        
+        // Testing the new constructor
+        Console.WriteLine($"myInvoice2's information is:\nPart number: {myInvoice2.Number}\nDescription: {myInvoice2.Description}\nQuantity: {myInvoice2.Quantity}\nPrice per item: {myInvoice2.Price:C}");
+
+        myInvoice2.Price = 5.55m;
+        Console.WriteLine($"myInvoice2's new price is: {myInvoice2.Price:C}");
+
+        Console.WriteLine($"Invoice amount is: {myInvoice2.GetInvoiceAmount():C}");
+    }
+  
+}
